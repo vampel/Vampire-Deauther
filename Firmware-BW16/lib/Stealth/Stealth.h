43c5ec56ac0edf107e9673af11f25a9b1da9456a
vampel <40074127+vampel@users.noreply.github.com>
2025-03-25 23:41:35 -0700

    Update Stealth.h

--- Firmware-BW16/lib/Stealth/Stealth.h
+++ Firmware-BW16/lib/Stealth/Stealth.h
@@ -10,3 +10,20 @@ public:
 private:
     static uint8_t generate_random_byte();
 };
+
+#pragma once
+#include <WiFi.h>
+#include <esp_wifi.h>
+
+class Stealth {
+public:
+    static void enable(uint8_t intensity = 3); // 1-5 (stealth level)
+    static void randomize_identity();
+    static void set_dynamic_intervals(bool enable);
+    
+    // Anti-Forensics
+    static void inject_dummy_traffic();
+    static void avoid_dfs_channels();
+private:
+    static uint8_t generate_crypto_byte();
+};
