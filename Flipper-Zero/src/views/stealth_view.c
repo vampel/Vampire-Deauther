440ab379a3a57eb76eb9f0e32a3a2d826e5d2ce8
vampel <40074127+vampel@users.noreply.github.com>
2025-03-26 00:07:51 -0700

    Delete Flipper-Zero/src/views directory

--- Flipper-Zero/src/views/stealth_view.c
+++
@@ -1,9 +0,0 @@
-static void stealth_view_draw_callback(Canvas* canvas, void* ctx) {
-    canvas_draw_str(canvas, 10, 20, "MODO STEALTH");
-    canvas_draw_icon(canvas, 30, 30, &I_stealth_icon);
-    canvas_draw_str(canvas, 60, 40, "ACTIVO");
-    
-    // Muestra MAC aleatoria actual
-    canvas_draw_str(canvas, 10, 60, "MAC:");
-    canvas_draw_str(canvas, 40, 60, get_current_mac());
-}
