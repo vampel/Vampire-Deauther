9a9408b393e08dea7285300806ef6c17819ca357
vampel <40074127+vampel@users.noreply.github.com>
2025-03-25 23:42:00 -0700

    Update Stealth.cpp

--- Firmware-BW16/lib/Stealth/Stealth.cpp
+++ Firmware-BW16/lib/Stealth/Stealth.cpp
@@ -23,3 +23,17 @@ void Stealth::spoof_wifi_probes() {
 uint8_t Stealth::generate_random_byte() {
     return (uint8_t)(esp_random() % 256);
 }
+
+
+void Stealth::randomize_identity() {
+    // MAC aleatoria válida (locally administered)
+    uint8_t mac[6] = {0x02, generate_crypto_byte(), generate_crypto_byte(), 
+                      generate_crypto_byte(), generate_crypto_byte(), generate_crypto_byte()};
+    wifi_set_macaddr(SOFTAP_IF, mac);
+    wifi_set_macaddr(STATION_IF, mac);
+}
+
+void Stealth::inject_dummy_traffic() {
+    uint8_t dummy_frame[26] = {0x80, 0x00}; // Beacon frame
+    wifi_send_pkt_freedom(dummy_frame, sizeof(dummy_frame), 0);
+}
