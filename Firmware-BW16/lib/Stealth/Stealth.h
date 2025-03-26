58fbcd5af2266dd948e919cce929193e3d73f96a
vampel <40074127+vampel@users.noreply.github.com>
2025-03-25 23:43:47 -0700

    Update Stealth.h

--- Firmware-BW16/lib/Stealth/Stealth.h
+++ Firmware-BW16/lib/Stealth/Stealth.h
@@ -1,29 +1,13 @@
 #pragma once
-#include <Arduino.h>
-
-class Stealth {
-public:
-    static void enable();  // Activa todas las técnicas stealth
-    static void randomize_mac();  
-    static void set_random_intervals();
-    static void spoof_wifi_probes();
-private:
-    static uint8_t generate_random_byte();
-};
-
-#pragma once
 #include <WiFi.h>
-#include <esp_wifi.h>
 
 class Stealth {
 public:
-    static void enable(uint8_t intensity = 3); // 1-5 (stealth level)
-    static void randomize_identity();
-    static void set_dynamic_intervals(bool enable);
+    static void enable(uint8_t level = 3);
+    static void randomize_mac();  // Unificada con randomize_identity()
     
-    // Anti-Forensics
-    static void inject_dummy_traffic();
-    static void avoid_dfs_channels();
+    // Anti-Detección
+    static void set_dynamic_delays();
 private:
-    static uint8_t generate_crypto_byte();
+    static uint8_t generate_secure_byte();  // Única función de generación
 };
