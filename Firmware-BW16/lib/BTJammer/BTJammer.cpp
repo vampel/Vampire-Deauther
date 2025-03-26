7d53fa2a37465263415f5a03c69224361fba323a
vampel <40074127+vampel@users.noreply.github.com>
2025-03-25 23:33:43 -0700

    Update BTJammer.cpp

--- Firmware-BW16/lib/BTJammer/BTJammer.cpp
+++ Firmware-BW16/lib/BTJammer/BTJammer.cpp
@@ -4,3 +4,14 @@ void BTJammer::start() {
   BLEServer *pServer = BLEDevice::createServer();
   pServer->getAdvertising()->start();
 }
+
+void BTJammer::start() {
+    WiFi.mode(WIFI_OFF); // Libera antena
+    BLEDevice::init("Fake Device");
+    while (true) {
+        BLEServer *pServer = BLEDevice::createServer();
+        pServer->getAdvertising()->setMinInterval(20); // Spam rápido
+        pServer->getAdvertising()->start();
+        delay(50);
+    }
+}
