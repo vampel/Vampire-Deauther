19eae2d6d33942368533f13543f84c5df6a0aab1
vampel <40074127+vampel@users.noreply.github.com>
2025-03-25 22:55:05 -0700

    Update main.cpp

--- Firmware-BW16/src/main.cpp
+++ Firmware-BW16/src/main.cpp
@@ -1,20 +1,14 @@
-#include <WiFi.h>
-#include <BLEDevice.h>
-#include "commands.h"
 #include "WiFiDeauth.h"
 #include "BTJammer.h"
 
 void setup() {
   Serial.begin(115200);
-  WiFi.mode(WIFI_OFF);
-  BLEDevice::init(""); // Inicializa BLE
+  if (!WiFiDeauth::init() || !BTJammer::init()) {
+    Serial.println("[ERROR] Inicialización fallida");
+    while(1);
+  }
 }
 
 void loop() {
-  if (Serial.available()) {
-    String cmd = Serial.readStringUntil('\n');
-    if (cmd == CMD_DEAUTH_24GHZ) WiFiDeauth::start(false); // 2.4GHz
-    else if (cmd == CMD_DEAUTH_5GHZ) WiFiDeauth::start(true); // 5GHz
-    else if (cmd == CMD_BT_JAMMER) BTJammer::start();
-  }
+  // Espera comandos UART desde Flipper
 }
