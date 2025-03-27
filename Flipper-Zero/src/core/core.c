d8b65d15449525d082bce1f118c13473c4078adf
vampel <40074127+vampel@users.noreply.github.com>
2025-03-26 17:56:17 -0700

    Update core.c

--- Flipper-Zero/src/core/core.c
+++ Flipper-Zero/src/core/core.c
@@ -14,3 +14,7 @@ void core_init() {
     view_port_draw_callback_set(view_port, ui_draw_callback, NULL);
     view_port_input_callback_set(view_port, handle_input);
 }
+
+
+uart_send_command("DEAUTH 2.4GHz STEALTH");
+uart_send_command("BTJAM RANDOM_MAC");
