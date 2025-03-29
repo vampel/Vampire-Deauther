c8bc96500d912c3bf7539d3702b87d47d7f9e0ae
vampel <40074127+vampel@users.noreply.github.com>
2025-03-29 00:30:08 -0700

    Update config.h

--- BW16-Firmware/config.h
+++ BW16-Firmware/config.h
@@ -1,25 +1,36 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-// Configuración de Hardware
-#define SERIAL_BAUDRATE 115200
-#define MAX_NETWORKS 100
+// Configuración del Hardware BW16
+#define SERIAL_BAUDRATE 115200  // Velocidad para comunicación con Flipper Zero
+#define CHANNEL_HOP_INTERVAL 200 // ms entre cambios de canal
 
-// Estados del sistema
-#define SCAN_INACTIVE 0
-#define SCAN_ACTIVE 1
-#define ATTACK_INACTIVE 0
-#define ATTACK_ACTIVE 1
+// Límites del sistema
+#define MAX_SCAN_RESULTS 150     // Máximo redes a almacenar
+#define MAX_SSID_LENGTH 32       // Longitud máxima para SSIDs
+#define DEAUTH_PACKET_RATE 100   // ms entre paquetes de deauth
 
-// Comandos desde Flipper Zero
-#define CMD_SCAN_START "SCAN"
-#define CMD_SCAN_STOP "SCAN_STOP"
-#define CMD_ATTACK_DEAUTH "DEAUTH"
-#define CMD_ATTACK_BEACON "BEACON"
-#define CMD_ATTACK_STOP "STOP"
+// Comandos para Flipper Zero (prefijo FLP:)
+#define FLP_CMD_SCAN_START "scan.start"
+#define FLP_CMD_SCAN_STOP "scan.stop"
+#define FLP_CMD_SCAN_RESULTS "scan.results"
+#define FLP_CMD_ATTACK_DEAUTH "attack.deauth"
+#define FLP_CMD_ATTACK_STOP "attack.stop"
+#define FLP_CMD_BEACON_SPAM "beacon.spam"
 
-// Canales WiFi optimizados
-const uint8_t CHANNELS_2GHZ[] = {1, 6, 11};
-const uint8_t CHANNELS_5GHZ[] = {36, 44, 149, 157};
+// Canales WiFi optimizados (2.4GHz y 5GHz)
+const uint8_t CHANNELS_2GHZ[] = {1, 6, 11};  // Canales no superpuestos
+const uint8_t CHANNELS_5GHZ[] = {36, 44, 48, 149, 153, 157, 161, 165};
 
-#endif
+// Tipos de ataque
+#define ATTACK_TYPE_DEAUTH 0x01
+#define ATTACK_TYPE_BEACON 0x02
+
+// Estructura para configuración de red
+typedef struct {
+    uint8_t channel;
+    bool is_5ghz;
+    uint8_t bssid[6];
+} wifi_target_t;
+
+#endif // CONFIG_H
