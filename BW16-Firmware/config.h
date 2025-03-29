2d0c1f057b87c28b8d3f7cdd1b2f4bdceb00a9d3
vampel <40074127+vampel@users.noreply.github.com>
2025-03-29 00:10:43 -0700

    Update config.h

--- BW16-Firmware/config.h
+++ BW16-Firmware/config.h
@@ -1,19 +1,25 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-// Configuración de hardware
+// Configuración de Hardware
 #define SERIAL_BAUDRATE 115200
-#define MAX_SCAN_NETWORKS 100
-#define MAX_SSID_LENGTH 32
+#define MAX_NETWORKS 100
 
-// Configuración de WiFi
-#define CHANNEL_HOP_INTERVAL 200 // ms
+// Estados del sistema
+#define SCAN_INACTIVE 0
+#define SCAN_ACTIVE 1
+#define ATTACK_INACTIVE 0
+#define ATTACK_ACTIVE 1
 
-// Comandos seriales para Flipper Zero
-#define CMD_SCAN_START "SCAN_START"
+// Comandos desde Flipper Zero
+#define CMD_SCAN_START "SCAN"
 #define CMD_SCAN_STOP "SCAN_STOP"
-#define CMD_ATTACK_DEAUTH "ATTACK_DEAUTH"
-#define CMD_ATTACK_BEACON "ATTACK_BEACON"
-#define CMD_SELECT_TARGET "SELECT_TARGET"
+#define CMD_ATTACK_DEAUTH "DEAUTH"
+#define CMD_ATTACK_BEACON "BEACON"
+#define CMD_ATTACK_STOP "STOP"
+
+// Canales WiFi optimizados
+const uint8_t CHANNELS_2GHZ[] = {1, 6, 11};
+const uint8_t CHANNELS_5GHZ[] = {36, 44, 149, 157};
 
 #endif
