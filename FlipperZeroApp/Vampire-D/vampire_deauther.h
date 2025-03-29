01a863c340cd9dd50a1aaa8bb810392e1ce38948
vampel <40074127+vampel@users.noreply.github.com>
2025-03-28 23:58:59 -0700

    Update vampire_deauther.h

--- FlipperZeroApp/Vampire-D/vampire_deauther.h
+++ FlipperZeroApp/Vampire-D/vampire_deauther.h
@@ -1,21 +1,19 @@
 #pragma once
 
 #include <furi.h>
-#include <furi_hal.h>
 #include <gui/gui.h>
 #include <gui/view_dispatcher.h>
 #include <gui/modules/submenu.h>
-#include <gui/modules/widget.h>
-#include <notification/notification.h>
+#include <gui/modules/dialog_ex.h>
+#include <notification/notification_messages.h>
 
 typedef struct {
     Gui* gui;
-    NotificationApp* notifications;
     ViewDispatcher* view_dispatcher;
     Submenu* submenu;
-    Widget* attack_view;
-    FuriHalSerialHandle* serial_handle;
-    bool attack_active;
+    NotificationApp* notifications;
 } VampireDeautherApp;
 
-int32_t vampire_deauther_app(void* p);
+typedef enum {
+    VampireDeautherViewSubmenu,
+} VampireDeautherView;
