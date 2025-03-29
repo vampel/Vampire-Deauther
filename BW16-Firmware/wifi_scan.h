08a5b06700e32f319306d05b176301bd548eb889
vampel <40074127+vampel@users.noreply.github.com>
2025-03-29 00:11:09 -0700

    Update wifi_scan.h

--- BW16-Firmware/wifi_scan.h
+++ BW16-Firmware/wifi_scan.h
@@ -1,8 +1,8 @@
-#ifndef WIFI_SCAN_H
-#define WIFI_SCAN_H
+#ifndef WIFI_SCANNER_H
+#define WIFI_SCANNER_H
 
-#include <WiFi.h>
 #include <vector>
+#include "config.h"
 
 struct WiFiNetwork {
   String ssid;
@@ -12,12 +12,13 @@ struct WiFiNetwork {
   bool is_5ghz;
 };
 
-extern std::vector<WiFiNetwork> networks;
-extern bool scan_active;
+extern std::vector<WiFiNetwork> wifi_networks;
+extern uint8_t scan_status;
 
-void init_wifi_scan();
+void init_wifi_scanner();
 void perform_wifi_scan();
-void start_scan();
-void stop_scan();
+void start_wifi_scan();
+void stop_wifi_scan();
+String get_scan_results_json();
 
 #endif
