bbbd43483dd3da580799b1cc1e344d3f1ed1f778
vampel <40074127+vampel@users.noreply.github.com>
2025-03-29 00:03:01 -0700

    Create serial_protocol.h

---
+++ BW16-Firmware/serial_protocol.h
@@ -0,0 +1,10 @@
+// serial_protocol.h
+#ifndef SERIAL_PROTOCOL_H
+#define SERIAL_PROTOCOL_H
+
+void init_serial_protocol();
+void handle_serial_commands();
+void send_scan_results();
+void send_attack_status();
+
+#endif
