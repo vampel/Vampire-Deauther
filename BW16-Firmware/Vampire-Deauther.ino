dbe1193d622374b966d2e9159af06c529962630c
vampel <40074127+vampel@users.noreply.github.com>
2025-03-28 23:56:28 -0700

    Update Vampire-Deauther.ino

--- BW16-Firmware/Vampire-Deauther.ino
+++ BW16-Firmware/Vampire-Deauther.ino
@@ -1,19 +1,31 @@
-#include <WiFi.h>
-#include <BLEDevice.h>
-#include "commands.h"
-
-#define UART_BAUDRATE 115200
+#include "config.h"
+#include "wifi_scan.h"
+#include "wifi_attack.h"
+#include "serial_protocol.h"
+#include "flipper_ui.h"
 
 void setup() {
-  Serial.begin(UART_BAUDRATE);  // UART con Flipper
+  Serial.begin(SERIAL_BAUDRATE);
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
-  BLEDevice::init("VampireBT"); // Inicializar BLE
+  
+  init_serial_protocol();
+  init_flipper_ui();
+  
+  Serial.println("Vampire-Deauther BW16 Ready");
 }
 
 void loop() {
-  if (Serial.available()) {
-    String command = Serial.readStringUntil('\n');
-    handleCommand(command);
+  handle_serial_commands();
+  update_flipper_ui();
+  
+  if(scan_active) {
+    perform_wifi_scan();
   }
+  
+  if(attack_active) {
+    perform_attack();
+  }
+  
+  delay(10);
 }
