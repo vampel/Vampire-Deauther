ecd23d1b0e4301b338ea005adbe2ebe0df368dd5
vampel <40074127+vampel@users.noreply.github.com>
2025-03-29 00:31:39 -0700

    Update flipper_serial.h

--- BW16-Firmware/flipper_serial.h
+++ BW16-Firmware/flipper_serial.h
@@ -1,12 +1,8 @@
 #ifndef FLIPPER_SERIAL_H
 #define FLIPPER_SERIAL_H
 
-#include "config.h"
-#include "wifi_scanner.h"
-#include "wifi_attack.h"
-
-void init_flipper_serial();
-void handle_flipper_commands();
-void send_to_flipper(const String& message);
+void initFlipperSerial();
+void handleFlipperSerial();
+void sendToFlipper(const String& message);
 
 #endif
