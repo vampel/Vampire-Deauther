3a37c6287bd6ddbf4bf636af27575f4ed3d0af9a
vampel <40074127+vampel@users.noreply.github.com>
2025-03-29 00:49:05 -0700

    Update flipper_serial.h

--- BW16-Firmware/flipper_serial.h
+++ BW16-Firmware/flipper_serial.h
@@ -1,10 +1,8 @@
 #ifndef FLIPPER_SERIAL_H
 #define FLIPPER_SERIAL_H
 
-#include "config.h"
-
 void init_flipper_serial();
 void handle_flipper_serial();
-void send_to_flipper(const String& msg);
+void send_to_flipper(const String& data);
 
 #endif
