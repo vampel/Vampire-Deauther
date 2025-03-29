fffa8264d7cd3f8ac09da1611c548752fb5b9ea3
vampel <40074127+vampel@users.noreply.github.com>
2025-03-29 00:45:13 -0700

    Update config.h

--- BW16-Firmware/config.h
+++ BW16-Firmware/config.h
@@ -4,17 +4,14 @@
 // Configuración BW16
 #define SERIAL_BAUDRATE 115200
 #define MAX_NETWORKS 150
-#define DEAUTH_RATE_MS 100
+#define SCAN_INTERVAL 200
 
-// Comandos Flipper Zero
-#define CMD_SCAN_START "scan_start"
-#define CMD_SCAN_STOP "scan_stop"
-#define CMD_DEAUTH "deauth"
-#define CMD_BEACON "beacon"
-#define CMD_STOP "stop"
+// Comandos Flipper
+#define FLIPPER_CMD_SCAN_START "scan_start"
+#define FLIPPER_CMD_ATTACK_DEAUTH "deauth"
 
-// Canales WiFi optimizados
-const uint8_t CH_2GHZ[] = {1, 6, 11};
-const uint8_t CH_5GHZ[] = {36, 44, 48, 149, 153, 157, 161, 165};
+// Canales optimizados
+const uint8_t CHANNELS_2GHZ[] = {1, 6, 11};
+const uint8_t CHANNELS_5GHZ[] = {36, 44, 48, 149, 153, 157};
 
 #endif
