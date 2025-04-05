deb4de9fffa37fac3df38b4d8ee08d9168647144
vampel <40074127+vampel@users.noreply.github.com>
2025-04-05 13:48:22 -0700

    Add files via upload

---
+++ applications_user/vampire_deauther/bw16_commands.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// Comandos básicos para el BW16
+#define BW16_CMD_SCAN_AP    "SCANAP\n"
+#define BW16_CMD_SCAN_STA   "SCANSTA\n"
+#define BW16_CMD_DEAUTH     "DEAUTH\n"
+#define BW16_CMD_SELECT     "SELECT\n"
+#define BW16_CMD_START      "START\n"
+#define BW16_CMD_STOP       "STOP\n"
+#define BW16_CMD_GET_STATUS "STATUS\n"
+
+// Timeouts en ms
+#define BW16_ACK_TIMEOUT      200
+#define BW16_RESPONSE_TIMEOUT 1000
