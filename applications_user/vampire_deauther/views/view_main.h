e72065d63141f27c01c928f7d35b7c00ab5c5531
vampel <40074127+vampel@users.noreply.github.com>
2025-04-15 00:00:39 -0700

    Update view_main.h

--- applications_user/vampire_deauther/views/view_main.h
+++ applications_user/vampire_deauther/views/view_main.h
@@ -1,18 +1,5 @@
 #pragma once
 
-#include <gui/gui.h>
 #include <gui/view.h>
-#include <furi_hal_serial.h>
-#include "../bw16_commands.h"
 
-typedef struct {
-    size_t selected_index;
-    bool scanning;
-    bool attacking;
-    uint8_t ap_count;
-    uint8_t sta_count;
-} VampireDeautherModel;
-
-bool input_callback(InputEvent* event, void* context);
-void render_callback(Canvas* canvas, void* context);
 View* vampire_deauther_view_get(void);
