ca37317e03d0f5bddaca1a779b1efcbf355914b8
vampel <40074127+vampel@users.noreply.github.com>
2025-04-14 23:59:10 -0700

    Update vampire_deauther.h

--- applications_user/vampire_deauther/vampire_deauther.h
+++ applications_user/vampire_deauther/vampire_deauther.h
@@ -1,18 +1,4 @@
 #pragma once
+#include <stdint.h>
 
-#include <gui/gui.h>
-#include <gui/view_dispatcher.h>
-#include <furi_hal_serial.h>
-#include <furi_hal_gpio.h>
-#include "bw16_commands.h"
-
-typedef struct {
-    Gui* gui;
-    ViewDispatcher* view_dispatcher;
-    View* main_view;
-    FuriHalSerialHandle* serial_handle;
-    bool bw16_ready;
-} VampireDeautherApp;
-
-void bw16_init_serial(VampireDeautherApp* app);
-bool bw16_send_command(VampireDeautherApp* app, const char* command);
+int32_t vampire_deauther_app(void* p);
