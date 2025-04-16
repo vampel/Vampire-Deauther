47d876cbfb1372b846f493a8d40cabba4d3e34a2
vampel <40074127+vampel@users.noreply.github.com>
2025-04-16 00:48:38 -0700

    Update view_main.h

--- applications_user/vampire_deauther/views/view_main.h
+++ applications_user/vampire_deauther/views/view_main.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#include <gui/view.h>
+#include <gui/view_dispatcher.h>  // para ViewDispatcher
 
-View* vampire_deauther_view_get(void);
+View* vampire_deauther_view_get_ex(ViewDispatcher* dispatcher);
