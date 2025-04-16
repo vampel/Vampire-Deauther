80672cc2518a869304623d4100ea21c6d569db9d
vampel <40074127+vampel@users.noreply.github.com>
2025-04-16 00:49:15 -0700

    Update vampire_deauther.c

--- applications_user/vampire_deauther/vampire_deauther.c
+++ applications_user/vampire_deauther/vampire_deauther.c
@@ -1,7 +1,7 @@
 #include <furi.h>
 #include <gui/gui.h>
 #include <gui/view_dispatcher.h>
-#include "views/view_main.h"
+#include "views/view_main.h"  // Asegúrate que este header tenga la nueva función
 
 int32_t vampire_deauther_app(void* p) {
     UNUSED(p);
@@ -13,15 +13,15 @@ int32_t vampire_deauther_app(void* p) {
     // Adjuntar dispatcher a GUI
     view_dispatcher_attach_to_gui(view_dispatcher, gui, ViewDispatcherTypeFullscreen);
 
-    // Crear vista principal
-    View* main_view = vampire_deauther_view_get();
+    // Obtener vista principal pasando el dispatcher
+    View* main_view = vampire_deauther_view_get_ex(view_dispatcher);
     view_dispatcher_add_view(view_dispatcher, 0, main_view);
     view_dispatcher_switch_to_view(view_dispatcher, 0);
 
-    // Ejecutar la vista
+    // Ejecutar la vista (bloquea hasta que se llame view_dispatcher_stop)
     view_dispatcher_run(view_dispatcher);
 
-    // Limpiar
+    // Limpiar recursos
     view_dispatcher_remove_view(view_dispatcher, 0);
     view_free(main_view);
     view_dispatcher_free(view_dispatcher);
